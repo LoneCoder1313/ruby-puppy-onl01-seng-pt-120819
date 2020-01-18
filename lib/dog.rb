c0d030e95aeecb1723fe7f6c045cc634053d9906
Ricky dildy <rdildy92@gmail.com>
2020-01-18 05:11:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,38 +1,8 @@
-# class Dog
-
-# 	attr_accessor :name
-
-# 	@@all = []
-
-# 	def initialize(name)
-# 		@name = name
-# 		@@all << self
-# 	end
-
-# def all_dogs(all)
-#     self.all = all_dogs 
-#     @@all << self 
-# 		end 
-# 		def print_all(all_dog_name)
-# 		  self. print_all = all_dog_name
-# 		  print_all.each { |all_dog_name| puts all_dog_name.name }
-# 		end 
-		
-# 			def save_dog_names(save) 
-# 	   self.save = save 
-# 	   @save = save 
-# 	end
-		
-# 	def self.clear_all
-# 		@@all.clear
-# 	end
-
-# 	end 
-
 
 class Dog
 
 	@@all = []
+	
 	attr_accessor :name
 
 	def initialize(name)
