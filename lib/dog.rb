4c53e5221f737ea86f006d114fb24ca88d492eab
Ricky dildy <rdildy92@gmail.com>
2020-01-18 04:55:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,9 +8,9 @@ class Dog
 		@name = name
 		@@all << self
 	end
-def self.all
-  @@all = all 
-  
+def all_dog_names(all)
+     self.all = all 
+     @@all = all 
 		end 
 		def self.print_all 
 		  print_all.each { |dog_name| puts dog_name.name }
